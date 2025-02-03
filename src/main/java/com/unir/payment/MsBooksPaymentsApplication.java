a6ae19a570ebf84aee4580a77f1e15ac00b9a721
yroyero <yroyero@gmail.com>
2025-02-03 00:23:03 -0300

    init

---
+++ src/main/java/com/unir/payment/MsBooksPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.unir.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBooksPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBooksPaymentsApplication.class, args);
+	}
+
+}
